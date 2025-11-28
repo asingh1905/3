be0993c826e9a05ee3ff4424ffe7159256fdcb59
Anshuman Singh <105singhanshuman@gmail.com>
2025-11-28 10:43:12 +0530

    bug fixed

--- shell_compare.sh
+++ shell_compare.sh
@@ -1,48 +1,15 @@
 #!/bin/bash
 
-# Read two strings from user
 echo "Enter first string: "
 read str1
 
 echo "Enter second string: "
 read str2
 
-# 1. Check if equal
 if [ "$str1" = "$str2" ]; then
-    echo "1. Strings are EQUAL"
+    echo "Strings are EQUAL"
+elif [ "$str1" \> "$str2" ]; then
+    echo "'$str1' is GREATER"
 else
-    echo "1. Strings are NOT equal"
-fi
-
-# 2. Check if first is greater (lexicographically)
-if [ "$str1" \> "$str2" ]; then
-    echo "2. '$str1' > '$str2'"
-elif [ "$str1" \< "$str2" ]; then
-    echo "2. '$str1' < '$str2'"
-else
-    echo "2. Strings are equal"
-fi
-
-# 3. Check lengths
-len1=${#str1}
-len2=${#str2}
-echo "3. Length of '$str1' = $len1"
-echo "   Length of '$str2' = $len2"
-
-if [ $len1 -gt $len2 ]; then
-    echo "   '$str1' is longer"
-elif [ $len1 -lt $len2 ]; then
-    echo "   '$str2' is longer"
-else
-    echo "   Both strings have same length"
-fi
-
-# 4. Case insensitive comparison (convert to lowercase first)
-str1_lower=$(echo "$str1" | tr '[:upper:]' '[:lower:]')
-str2_lower=$(echo "$str2" | tr '[:upper:]' '[:lower:]')
-
-if [ "$str1_lower" = "$str2_lower" ]; then
-    echo "4. Strings are EQUAL (case insensitive)"
-else
-    echo "4. Strings are different (case insensitive)"
+    echo "'$str2' is GREATER"
 fi
